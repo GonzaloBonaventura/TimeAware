1e9cac8955c22764d91f3a796607200bb7e788b8
GonzaloBonaventura <66089755+GonzaloBonaventura@users.noreply.github.com>
2021-11-04 20:33:47 -0300

    Fix
    Multiple Timecheckers are no longer able to be started.
    Initial timing fixed.
    Fast shutdown implemented.

--- Funciones/timechecker.js
+++ Funciones/timechecker.js
@@ -1,49 +1,55 @@
 var active = false;
+var shutdown = true;
 var options = { hour12: false };
 async function timeaware() {
+  shutdown = false;
 	console.log("start confirmation");
 	var time = getTime();
 	var min=time.getMinutes();
 	var sec=time.getSeconds();
 	console.log(min)
 	var out;
-	/*
-	old way to align with quarters:
-	while(min != 0 && min != 15 && min != 30 && min != 45  && min != 0){
-		time = getTime();
-		await sleep(60000);
-		min=time.getMinutes();
-		console.log(min);
-	}
-	*/
 	var wait = 0;
 	if (min != 0 && min != 15 && min != 30 && min != 45) {
 		if(min<15){
+			console.log("if15");
 			wait = (15 - min)*60000 - sec * 1000;
-		}
-		if(min<30){
-			wait = (30 - min)*60000 - sec * 1000;
-		}
-		if(min<45){
-			wait = (45 - min)*60000 - sec * 1000;
-		}
-		if(min<60){
-			wait = (60 - min)*60000 - sec * 1000;
+		}else{
+			if(min<30){
+					console.log("if30");
+					wait = (30 - min)*60000 - sec * 1000;
+			}else{
+				if(min<45){
+					console.log("if45");
+					wait = (45 - min)*60000 - sec * 1000;
+				}else{
+					if(min<60){
+						console.log("if60");
+						wait = (60 - min)*60000 - sec * 1000;
+					}
+				}
+			}
 		}
 	}
 	console.log(min + " . wait= " + wait);
+	var waitmin = Math.round(wait/60000);
+	console.log("minwait= " + waitmin + ". segwait:" + (wait/1000)%60);
+
 	if (active == true) {
-		await sleep(wait);
+		//await sleep(wait);
+		await  sleepwhile(wait/1000);
 		ttsi(hournmin());
 	}else{
 		document.getElementById('light').className = "red";
 		console.log("stop confirmation");
+		shutdown = true;
 		return;
 	}
 	document.getElementById('light').className = "green";
 
 	while(active){
-		await sleep(900000);
+		//await sleep(900000);
+		await  sleepwhile(900);
 		if (active == true) {
 			out = ttsi(hournmin());
 			console.log(out);
@@ -51,13 +57,17 @@ async function timeaware() {
 	}
 	document.getElementById('light').className = "red";
 	console.log("stop confirmation");
+	shutdown = true;
 }
 async function hournmin(){
 	var time = getTime();
 	var hora = time.getHours();
 	var minu = time.getMinutes();
+	if (minu < 10) {
+		minu = "0" + minu;
+	}
 	console.log(hora + ':' + minu);
-	return 'son las ' + hora + ':' + minu
+	return hora + ':' + minu
 }
  function getTime(){
 	var time = Date.now();
@@ -70,24 +80,37 @@ function humantime() {
 	console.log((time = date.toLocaleString('en-US', options)));
 	document.title = time;
 }
-
 async function timestart() {
-	active = true;
+	if (shutdown == true) {
 		if(document.getElementById('light').className == "" || document.getElementById('light').className == "red"){
-		document.getElementById('light').className = "lgreen";
+				document.getElementById('light').className = "lgreen";
+		}
+		active = true;
+		timeaware();
 	}
-	timeaware();
 }
-async function timestop() {
-	active = false;
-	if(document.getElementById('light').className != ""){
-		document.getElementById('light').className = "yellow";
+function timestop() {
+	if (shutdown == false) {
+		active = false;
+		if(document.getElementById('light').className != ""){
+			document.getElementById('light').className = "yellow";
+		}
 	}
+
 }
 async function parseDate(input) {
   var parts = input.match(/(\d+)/g);
   // new Date(year, month [, date [, hours[, minutes[, seconds[, ms]]]]])
   return new Date(parts[0], parts[1]-1, parts[2]); // months are 0-based
+}
+async function sleepwhile(s){
+	for (var i = 0; i < s; i++) {
+		await sleep(1000);
+		if (active == false) {
+			console.log("stopped");
+			return;
+		}
+	}
 }
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -97,23 +120,23 @@ function sleep(ms) {
     	console.log(result);
     	var base = "http://api.voicerss.org/?key=";
 		var key = "5ba7932e18814afc90c6f93048e002e5";
-		if (result == "son las 0:0") {
-			result = "Es medianoche";
-		}
 		var text = "&src=" + encodeURIComponent(result);
-		var lang = "&c=MP3&hl=es-" + document.getElementById('voice').value;
+		var lang = "&c=MP3&hl=" + document.getElementById('voice').value;
 
 		var link = base + key + text + lang;
-		play(link);
-		await sleep(10000);
-		play(link);
+		if (active == true) {
+			play(link);
+			await sleep(10000);
+			play(link);
+		}
 	});
 }
 function tryvoice(){
 		var base = "http://api.voicerss.org/?key=";
 		var key = "5ba7932e18814afc90c6f93048e002e5";
-		var text = "&src=Son%20las%2012%3A45";
-		var lang = "&c=MP3&hl=es-" + document.getElementById('voice').value;
+		console.log('test: 12:45, 8:00, 0:00, 16:30');
+		var text = "&src=" + encodeURIComponent("12:45, 8:00, 0:00, 16:30");
+		var lang = "&c=MP3&hl=" + document.getElementById('voice').value;
 
 		var link = base + key + text + lang;
 		play(link);
